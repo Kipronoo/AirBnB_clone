d7392918a2082db5d0dd681682b2ebb2215d655b
Kipronoo <136130745+Kipronoo@users.noreply.github.com>
2024-07-15 16:31:07 +0300

    Update __init__.py

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/python3
-"""
-Initialization module for the models package
-"""
-from models.engine.file_storage import FileStorage
 
-# Create an instance of FileStorage
-storage = FileStorage()
-storage.reload()
