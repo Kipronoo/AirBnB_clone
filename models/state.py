d994bac7059efb2cb0daf1c14ba877f39a849342
Kipronoo <136130745+Kipronoo@users.noreply.github.com>
2024-07-26 10:21:42 +0300

    state.py

--- models/state.py
+++ models/state.py
@@ -2,12 +2,9 @@
 """Defines the State class."""
 from models.base_model import BaseModel
 
-
 class State(BaseModel):
-    """Represent a state.
-
-    Attributes:
-        name (str): The name of the state.
-    """
-
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
